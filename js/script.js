b3ac547bb596c1f1ee04f11208077895f4d85e85
Nomi Vos <nvoswork@gmail.com>
2022-01-17 17:39:57 -0600

    forgot a semi-colon

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const img = document.querySelector(".img");
 const getImage = async function () {
   const res = await fetch (
     "https://picsum.photos/v2/list?limit=100"
-  )
+  );
   const images = await res.json ();
   console.log(images);
 };
